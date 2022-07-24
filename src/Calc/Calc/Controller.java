f329c5e4308d86b117cadfa5e3534ff6a8171286
DillTheBeast <dillonmaltese@gmail.com>
2022-07-24 19:22:01 -0400

    Initial commit

---
+++ src/Calc/Calc/Controller.java
@@ -0,0 +1,17 @@
+package Calc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.*;
+
+public class Controller {
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+    }
+}
